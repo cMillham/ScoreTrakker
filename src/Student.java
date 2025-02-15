a06504d14f94044d6c066c4f4387139a8680906c
cMillham <chloemillham@Chloes-MacBook-Pro-3.local>
2025-02-15 15:49:38 -0700

    Student

--- src/Student.java
+++ src/Student.java
@@ -1,23 +1,28 @@
-
+//Author: Chloe Millham, written 2/13/2025
+//Student Class: initializes name and score variables, allows student objects to be compared by name and sorted properly
+//also contains a toString() method to properly format the student's name and score
 public class Student implements Comparable<Student>{
 
 	private String name;
 	private int score;
 	
 	Student(String name, int score) {
-		name = this.name;
-		score = this.score;
+		this.name = name;
+		this.score = score;
 	}
 
 	@Override
-	public int compareTo(Student o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(Student other) {
+		int order = this.name.compareTo(other.name);
+		if(order == 0) {
+			return other.score - this.score;
+		}
+		return order;
 	}
 
 	@Override
 	public String toString() {
-		return "Student Score List\n" +  name + " " + score + "\n";
+		return  name + " " + score;
 	}
 	
 	
